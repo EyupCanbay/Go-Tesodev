7b2e2cc5b1ced1c32afbad8d656076f5303153aa
EyupCanbay <eyupcanbay40@gmail.com>
2025-05-09 23:28:43 +0300

    Add log middleware with sirupsen/logrus

--- main.go
+++ main.go
@@ -1,5 +1,28 @@
 package main
 
-func main() {
+import (
+	"os"
+	"tesodev/configs"
+	"tesodev/middleware"
 
+	"github.com/labstack/echo/v4"
+	"github.com/sirupsen/logrus"
+)
+
+func init() {
+	if os.Getenv("APP_ENV") == "development" {
+		logrus.SetLevel(logrus.DebugLevel)
+	} else {
+		logrus.SetLevel(logrus.WarnLevel)
+	}
+}
+
+func main() {
+	e := echo.New()
+
+	e.Use(middleware.LogMiddleware)
+
+	configs.ConnectDB()
+
+	e.Logger.Fatal(e.Start(":3000"))
 }
