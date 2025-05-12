5c1ff2962f014129358476a21cc27fb8b1b42b30
EyupCanbay <eyupcanbay40@gmail.com>
2025-05-12 22:19:43 +0300

    Add random data generator

---
+++ utils/random.go
@@ -0,0 +1,39 @@
+package utils
+
+import (
+	"math/rand"
+	"strings"
+	"time"
+)
+
+const alphabet = "qwertyuıopğüasdfghjklşizxcvbnmöç"
+
+// for true randomness if could not be init function every time
+//
+//	would generate same number because the function can generates
+//	 true randomness according to starting number so ı give from nanosecond
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+// random intager generates a random between min and max
+func RandomInt(min, max int64) int64 {
+	return min + rand.Int63n(max-min+1)
+}
+
+// RandonStirng generates a random sittring of length n
+func RandomString(n int) string {
+	var sb strings.Builder
+	k := len(alphabet)
+
+	for i := 0; i < n; i++ {
+		c := alphabet[rand.Intn(k)]
+		sb.WriteByte(c)
+	}
+	return sb.String()
+}
+
+// RandomMoney generates a random amount of money
+func RandomPrice() int64 {
+	return RandomInt(0, 1000)
+}
